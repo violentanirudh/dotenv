f1d689e0c78a86aec2d22e58821764a10ee3b2de
Anirudh Singh <violentanirudh@gmail.com>
2024-10-03 00:42:02 +0530

    Modified Background

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className='bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-950 h-screen w-screen relative text-slate-300 overflow-scroll'>
-      <div className="absolute inset-0 bg-zinc-950/60 bg-url['/graph-paper.svg'] overflow-scrolls">
-      <div className='h-screen overflow-y-scroll py-12 px-4 bg-[url("./graph-paper.svg")] bg-repeat'>
+      <div className="absolute inset-0 bg-zinc-950/60 overflow-scrolls">
+      <div className='h-screen overflow-y-scroll py-12 px-4 bg-[url("/graph-paper.svg")] bg-repeat'>
           <div className='max-w-screen-xl mx-auto'>
             <Navbar />  
             <HeroSection />
