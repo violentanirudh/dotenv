ef48277bb8e5bc4f429adba211638936508fae52
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2024-10-04 00:37:57 +0530

    PreLoader

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import GlowingCursor from './components/GlowingCursor';
+import PreLoader from './components/PreLoader';
 
 const Layout = () => {
+
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    if (navigation.state === 'loading') {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [navigation.state]);
+
   return (
     <GlowingCursor>
+      {loading ? (
+        <PreLoader />
+      ) : (
       <div className='bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-950 h-screen w-screen relative text-slate-300'>
         <div className="absolute inset-0 bg-zinc-950/60 overflow-y-scroll">
           <div className='py-12 px-4 bg-[url("/graph-paper.svg")] bg-repeat'>
@@ -17,6 +34,7 @@ const Layout = () => {
           </div>
         </div>
       </div>
+      )}
     </GlowingCursor>
   );
 }
---
+++ src/components/PreLoader.jsx
@@ -0,0 +1,9 @@
+const PreLoader = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-indigo-500"></div>
+        </div>
+    )
+}
+
+export default PreLoader;
