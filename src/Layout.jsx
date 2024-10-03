6ece62734ceefd245218df4887058c9ce7c8d52c
MilanGohel <milan.gohel66@gmail.com>
2024-10-03 18:28:02 +0530

    Resolve multiple scrollbar issue on large device

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,12 @@
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
+import "./layout.css"
 const Layout = () => {
   return (
-    <div className='bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-950 h-screen w-screen relative text-slate-300 overflow-scroll'>
-      <div className="absolute inset-0 bg-zinc-950/60 overflow-scroll">
-        <div className='h-screen overflow-y-scroll py-12 px-4 bg-[url("/graph-paper.svg")] bg-repeat'>
+    <div className='bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-950 h-screen w-screen relative text-slate-300 overflow-y-scroll overflow-x-hidden scroller '>
+      <div className="absolute inset-0 bg-zinc-950/60 overflow-hidden">
+        <div className='h-screen overflow-hidden py-12 px-4 bg-[url("/graph-paper.svg")] bg-repeat'>
           <div className='max-w-screen-xl mx-auto'>
             <Navbar />
             <Outlet />
