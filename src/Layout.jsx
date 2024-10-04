c5db2f0d41f379acf5f9686f00fa4af15829138b
Anirudh Singh <violentanirudh@gmail.com>
2024-10-04 23:46:43 +0530

    Bug

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet, useNavigate, Link as RouterLink, useLocation } from 'react-router-dom';
+import { FaTerminal } from 'react-icons/fa6';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import GlowingCursor from './components/GlowingCursor';
 import PreLoader from './components/PreLoader';
 import Terminal from './components/Terminal';
-import { TerminalIcon } from 'lucide-react';
 
 // Custom Link component
 const Link = ({ to, children, ...props }) => {
   const navigate = useNavigate();
-
   const handleClick = (e) => {
     e.preventDefault();
     window.scrollTo(0, 0);
     setTimeout(() => navigate(to), 50); // Small delay to ensure scroll completes
   };
-
   return (
     <RouterLink to={to} onClick={handleClick} {...props}>
       {children}
@@ -38,24 +36,24 @@ const Layout = () => {
   return (
     <GlowingCursor>
       <div className='bg-gradient-to-br from-slate-950 via-indigo-950 to-slate-950 h-screen w-screen relative text-slate-300'>
-          <div className="absolute inset-0 bg-zinc-950/60 overflow-y-scroll">
-            <div className='py-12 px-4 bg-[url("/graph-paper.svg")] bg-repeat min-h-screen'>
-              {loading ? (
-                <PreLoader />
-              ) : (
-                
-                <div className='max-w-screen-xl mx-auto'>
-                  <Navbar />
-                  <Outlet />
-                  <Footer />
-                  <Terminal isOpen={isTerminalOpen} setIsOpen={setIsTerminalOpen} /><button
-                    onClick={() => setIsTerminalOpen(!isTerminalOpen)}
-                    className="fixed bottom-8 right-8 bg-indigo-600 text-white p-2 rounded shadow-lg hover:bg-indigo-700 transition-colors duration-200 z-30"
-                  >
-                    <TerminalIcon size={24} />
-                  </button>
-                </div>
-              )}
+        <div className="absolute inset-0 bg-zinc-950/60 overflow-y-scroll">
+          <div className='py-12 px-4 bg-[url("/graph-paper.svg")] bg-repeat min-h-screen'>
+            {loading ? (
+              <PreLoader />
+            ) : (
+              <div className='max-w-screen-xl mx-auto'>
+                <Navbar />
+                <Outlet />
+                <Footer />
+                <Terminal isOpen={isTerminalOpen} setIsOpen={setIsTerminalOpen} />
+                <button
+                  onClick={() => setIsTerminalOpen(!isTerminalOpen)}
+                  className="fixed bottom-8 right-8 bg-indigo-600 text-white p-2 rounded shadow-lg hover:bg-indigo-700 transition-colors duration-200 z-30"
+                >
+                  <FaTerminal size={24} />
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
